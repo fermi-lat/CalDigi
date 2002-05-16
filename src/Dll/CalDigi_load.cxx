eca5a8504025765baeeb16f5fcee783a23d3dd6b
burnett <>
2002-05-16 00:04:43 +0000

    rename the dll files for Gaudi

--- src/Dll/CalDigi_load.cxx
+++ src/Dll/CalDigi_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/GlastDigi/src/Dll/GlastDigi_load.cxx,v 1.3 2002/04/23 23:28:27 richard Exp $
+*  $Header: $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
