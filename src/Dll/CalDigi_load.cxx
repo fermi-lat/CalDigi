f794637a8bfdc699ed8336ac8c7e211a5f2e7017
kyoung <>
2002-10-08 22:13:17 +0000

    Add CalDigiRandom tool

--- src/Dll/CalDigi_load.cxx
+++ src/Dll/CalDigi_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/CalDigi/src/Dll/CalDigi_load.cxx,v 1.1 2002/05/16 00:04:42 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/CalDigi/src/Dll/CalDigi_load.cxx,v 1.2 2002/08/18 16:09:53 richard Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -16,6 +16,6 @@ DECLARE_FACTORY_ENTRIES(CalDigi) {
     DECLARE_ALGORITHM( CalDigiAlg );
     DLL_DECL_TOOL( LinearTaper   );
     DLL_DECL_TOOL( OnePlusExpTaper   );
-
+    DLL_DECL_TOOL( CalDigiRandom   );
 } 
 
