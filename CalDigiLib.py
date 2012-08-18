18fb26e808bbef560dbe6cbe60087715b40acc45
jrb <>
2012-08-18 00:17:53 +0000

    support no-install headers on Windows

--- CalDigiLib.py
+++ CalDigiLib.py
@@ -1,4 +1,4 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalDigi/CalDigiLib.py,v 1.2 2008/08/25 19:37:58 ecephas Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalDigi/CalDigiLib.py,v 1.3 2009/08/25 23:20:16 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalDigi'])
@@ -7,5 +7,11 @@ def generate(env, **kw):
     env.Tool('CalUtilLib')
     env.Tool('xmlBaseLib')
     env.Tool('addLibrary', library = env['gaudiLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'CalXtalResponse') 
+        env.Tool('findPkgPath', package = 'LdfEvent') 
+        env.Tool('findPkgPath', package = 'Event') 
+        env.Tool('findPkgPath', package = 'enums') 
+	env.Tool('findPkgPath', package = 'ConfigSvc')
 def exists(env):
     return 1;
